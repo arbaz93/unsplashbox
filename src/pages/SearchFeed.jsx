580cc6fb81d58db6059df50b236197e50db93d9c
arbaz93 <arbaz.k480@yahoo.com>
2025-01-20 17:46:09 +0500

    added infinite scroll to searchFeed

--- src/pages/SearchFeed.jsx
+++ src/pages/SearchFeed.jsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { SearchForm, ImageCard } from "../components"
-import Masonry, { ResponsiveMasonry} from 'react-responsive-masonry';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { useEffect, useState } from "react";
 import { fetchFromAPI } from "../js/fetchFromAPI";
 
@@ -8,50 +9,64 @@ export default function SearchFeed() {
   const searchQuery = useLocation().state.searchQuery;
   const [images, setImages] = useState([])
   const [fetchStatus, setFetchStatus] = useState('fetching');
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    fetchFromAPI(`search/photos?query=${searchQuery}`)
-    .then(res => {
-      if(res.total != 0) {
-        setFetchStatus('200')
-        setImages(res.results.map(i => {
-          const IMAGE_DATA = {
-            id: i.id,
-            alt: i.alt_description,
-            created_at: i.created_at,
-            height: i.height,
-            width: i.width,
-            links: i.links,
-            urls: i.urls,
-            user: i.user
-          }
-          return IMAGE_DATA;
-        }));
-      } else {
-        setFetchStatus('empty')
-      }
-    })
-    .catch(error => {
-      console.error(error)
-    });
+    fetchImages();
   }, [])
 
   function handleClick() {
-    console.log(images)
+    console.log(fetchStatus)
+  }
+  function fetchImages() {
+    fetchFromAPI(`search/photos?query=${searchQuery}`, currentPage)
+      .then(res => {
+        console.log(res)
+        if (res.results.length != 0) {
+          setFetchStatus('200')
+          setImages(images.concat(res.results.map(i => {
+            const IMAGE_DATA = {
+              id: i.id,
+              alt: i.alt_description,
+              created_at: i.created_at,
+              height: i.height,
+              width: i.width,
+              links: i.links,
+              urls: i.urls,
+              user: i.user
+            }
+            return IMAGE_DATA;
+          })));
+        } else {
+          setFetchStatus('empty')
+        }
+      })
+      .catch(error => {
+        console.error(error)
+      });
+    setCurrentPage(currentPage + 1)
+    console.log(currentPage)
   }
   return (
-    <section className="flex flex-col items-center">
-      <div onClick={handleClick} className="pt-16 pb-16 half-gradient">
-        <SearchForm text={searchQuery}/>
+    <section className=" flex-col flex items-center">
+      <div onClick={handleClick} className="pt-16 pb-16 w-full flex justify-center half-gradient">
+        <SearchForm text={searchQuery} />
       </div>
-      {fetchStatus === 'fetching' ? <p>Loading!</p> 
-      : fetchStatus === 'empty' ? <p>{searchQuery} not found!</p> 
-      : <ResponsiveMasonry className="w-[80%]" columnsCountBreakPoints={{350: 2, 640: 3, 900: 4}}>
-        <Masonry gutter='1rem'>
-          
-        {images.map(imageData => <ImageCard key={imageData.id} imageData={imageData} />)}
-        </Masonry>
-      </ResponsiveMasonry>
+      {fetchStatus === 'fetching' ? <p>Loading!</p>
+        : (fetchStatus === 'empty' && images.length === 0) ? <p>{searchQuery} not found!</p>
+          : <ResponsiveMasonry className="w-[80%]" columnsCountBreakPoints={{ 350: 2, 640: 3, 900: 4 }}>
+            <InfiniteScroll
+              dataLength={images.length}
+              next={fetchImages}
+              hasMore={fetchStatus === 'empty' ? false : true}
+              loader={<h1>loadad</h1>}
+            >
+              <Masonry gutter='1rem'>
+
+                {images.map(imageData => <ImageCard key={imageData.id} imageData={imageData} />)}
+              </Masonry>
+            </InfiniteScroll>
+          </ResponsiveMasonry>
       }
     </section>
   )
